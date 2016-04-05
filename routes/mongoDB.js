b9c76a9a579992cc78c4c39e9b8971a1e4fc0041
Shawn <yushangzfds2008@163.com>
2016-04-05 01:01:26 -0400

    can't solve post array

--- routes/mongoDB.js
+++ routes/mongoDB.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
 var fs = require("fs");
 var MongoClient = require('mongodb').MongoClient;
@@ -13,35 +14,35 @@ var url = 'mongodb://localhost:27017/BankDB';
 // router.use('/database/post', postREST);
 // router.use('/database/del', delREST);
 /* GET users listing. */
-router.route('/add').get(function (req, res, next) {
-    console.log('requested');
-    MongoClient.connect(url, function (err, db) {
-        assert.equal(null, err);
-        console.log("Connected correctly to Mongo server");
-        console.log(req.query);
-        // Get the documents collection
-        var collection = db.collection('users');
-        // console.log(req.query[0]);
-        // collection.insertMany(JSON.parse(req.query));
-        var request = [];
-        for (x in req.query) {
-            request.push(JSON.parse(req.query[x]));
+router.route('/add')
+    .post(function (req, res, next) {
+        console.log('requested');
+        console.log(req.body);
+        MongoClient.connect(url, function (err, db) {
+            assert.equal(null, err);
+            console.log("Connected correctly to Mongo server");
+
+            // Get the documents collection
+            var collection = db.collection('users');
+            // console.log(req.query[0]);
+            // collection.insertMany(JSON.parse(req.query));
+            var request = [];
+            // for (x in req.body) {
+            // request.push(req.body[x]);
             console.log(request);
-        }
-        collection.insertMany(request);
-        db.close();
-        // fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
-        //     console.log(data);
-        //     res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
-        //     res.end(data);
-        // });
+            // }
+            collection.insert(req.body);
+            db.close();
+            // fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+            //     console.log(data);
+            //     res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
+            //     res.end(data);
+            // });
+        });
     });
-});
 
 router.route('/get').get(function (req, res, next) {
     // console.log(JSON.stringify(req));
-
-
     MongoClient.connect(url, function (err, db) {
         console.log("Connected correctly to Mongo server");
         // Get the documents collection
@@ -52,7 +53,7 @@ router.route('/get').get(function (req, res, next) {
                 console.log('Error:' + err);
                 return;
             }
-            console.log(result);
+            // console.log(result);
             db.close();
             res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
             res.end(JSON.stringify(result));
@@ -66,42 +67,43 @@ router.route('/get').get(function (req, res, next) {
     })
 });
 
-router.route('/del').all(function (req, res, next) {
-    console.log('del requested');
-    MongoClient.connect(url, function (err, db) {
-        assert.equal(null, err);
-        console.log("Connected correctly to Mongo server");
-        console.log(req.query);
-        // Get the documents collection
-        var collection = db.collection('users');
+router.route('/del')
+    .all(function (req, res, next) {
+        console.log('del requested');
+        MongoClient.connect(url, function (err, db) {
+            assert.equal(null, err);
+            console.log("Connected correctly to Mongo server");
+            console.log(req.query);
+            // Get the documents collection
+            var collection = db.collection('users');
 
-        for (x in req.query) {
-            console.log(req.query[x]);
-            var whereStr = {_id: new mongodb.ObjectID(req.query[x])};
-            // var BSON = require('mongodb').BSONPure;
-            // whereStr = {_id: BSON.ObjectID.createFromHexString('5700afc1ea82edf8359a1530')};
-            collection.remove(whereStr, function (err, result) {
-                if (err) {
-                    console.log('Error:' + err);
-                    return;
-                }
-                // console.log(result);
-                db.close();
+            for (x in req.query) {
+                console.log(req.query[x]);
+                var whereStr = {_id: new mongodb.ObjectID(req.query[x])};
+                // var BSON = require('mongodb').BSONPure;
+                // whereStr = {_id: BSON.ObjectID.createFromHexString('5700afc1ea82edf8359a1530')};
+                collection.remove(whereStr, function (err, result) {
+                    if (err) {
+                        console.log('Error:' + err);
+                        return;
+                    }
+                    // console.log(result);
+                    db.close();
+                });
+            }
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'DELETE'
             });
-        }
-        res.writeHead(200, {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'DELETE'
+            res.end();
+            // fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+            //     console.log(data);
+            //     res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
+            //     res.end(data);
+            // });
         });
-        res.end();
-        // fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
-        //     console.log(data);
-        //     res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
-        //     res.end(data);
-        // });
     });
-});
 
 router.post('/put', function (req, res, next) {
     // console.log(JSON.stringify(req));
